e6f5b00ff13cb4b82f57d1c3a476d94359a4d4d8
esh0729 <esh0729@gmail.com>
2022-07-06 10:08:34 +0900

    #. GameServer.HeroInitEnterCommandHandler 클래스/변수/함수 설명 추가

--- GameServer/Client/Handler/Command/Login/InGame/HeroInitEnterCommandHandler.cs
+++ GameServer/Client/Handler/Command/Login/InGame/HeroInitEnterCommandHandler.cs
@@ -7,11 +7,15 @@ using ClientCommon;
 
 namespace GameServer
 {
+	//=====================================================================================================================
+	// (InGameCommandHandler 상속) 영웅 로그인 이후 초기 입장에 대한 클라이언트 명령을 처리하는 클래스
+	//=====================================================================================================================
 	public class HeroInitEnterCommandHandler : InGameCommandHandler<HeroInitEnterCommandBody, HeroInitEnterResponseBody>
 	{
 		////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 		// Properties
 
+		// 영웅 객체를 장소 객체에서 추가 하기 때문에 상위 lock 객체인 Cache lock 처리 프로퍼티 true로 오버라이딩
 		protected override bool globalLockRequired
 		{
 			get { return true; }
@@ -20,10 +24,15 @@ namespace GameServer
 		////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 		// Member functions
 
+		//=====================================================================================================================
+		// 영웅 초기 입장 이후 클라이언트 응답 전송을 처리하는 함수
+		//=====================================================================================================================
 		protected override void OnInGameCommandHandle()
 		{
+			// 현재 시각
 			DateTimeOffset currentTime = DateTimeUtil.currentTime;
 
+			// 영웅 로그인때 처리 되었던 초기 입장 정보 확인
 			HeroInitEnterParam param = m_myHero.entranceParam as HeroInitEnterParam;
 
 			if (param == null)
@@ -37,19 +46,14 @@ namespace GameServer
 			if (continentInstance == null)
 				throw new CommandHandleException(kResult_Error, "대륙인스턴스가 존재하지 않습니다. continentId = " + continent.id);
 
-			//
-			// 입장
-			//
-
+			// 입장 처리하기 위해 해당 장소에 lock 처리
 			lock (continentInstance.syncObject)
 			{
+				// 위치 및 방향 수정 후 입장 처리
 				m_myHero.SetPosition(position, fYRotation);
 				continentInstance.Enter(m_myHero);
 
-				//
-				// 응답
-				//
-
+				// 응답 객체 생성
 				HeroInitEnterResponseBody resBody = new HeroInitEnterResponseBody();
 				resBody.placeInstanceId = continentInstance.instanceId;
 
@@ -58,6 +62,7 @@ namespace GameServer
 
 				resBody.heroes = Hero.ToPDHeroes(continentInstance.GetInterestedHeroes(m_myHero.sector, m_myHero.id)).ToArray();
 
+				// 클라이언트 응답 전송 
 				SendResponseOK(resBody);
 			}
 		}
