c111abd66aae43240bee034069ce9178784eb205
esh0729 <esh0729@gmail.com>
2022-07-07 11:48:09 +0900

    #. GameServer.ActionCommandHandler 클래스/변수/함수 설명 추가

--- GameServer/Client/Handler/Command/Login/InGame/Action/ActionCommandHandler.cs
+++ GameServer/Client/Handler/Command/Login/InGame/Action/ActionCommandHandler.cs
@@ -8,8 +8,14 @@ using ClientCommon;
 
 namespace GameServer
 {
+	//=====================================================================================================================
+	// (LoginRequiredCommandHandler 상속) 캐릭터 행동 클라이언트 명령을 처리하는 클래스
+	//=====================================================================================================================
 	public class ActionCommandHandler : InGameCommandHandler<ActionCommandBody, ActionResponseBody>
 	{
+		//=====================================================================================================================
+		// 캐릭터 행동 완료 이후 클라이언트 응답 전송을 처리하는 함수
+		//=====================================================================================================================
 		////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 		// Member functions
 
@@ -18,19 +24,13 @@ namespace GameServer
 			if (m_body == null)
 				throw new CommandHandleException(kResult_Error, "body가 null입니다.");
 
-			//
 			// 장소 검사
-			//
-
 			PhysicalPlace currentPlace = m_myHero.currentPlace;
 
 			if (currentPlace == null)
 				throw new CommandHandleException(kResult_Error, "현재 장소에서 사용할 수 없는 명령입니다.");
 
-			//
 			// 유효성 검사
-			//
-
 			int nActionId = m_body.actionId;
 			Vector3 position = m_body.position;
 			float fYRotation = m_body.yRotation;
@@ -45,28 +45,16 @@ namespace GameServer
 			if (!currentPlace.ContainsPosition(position))
 				throw new CommandHandleException(kResult_Error, "행동위치가 유효하지 않습니다. position = " + position);
 
-			//
 			// 이동 처리
-			//
-
 			currentPlace.MoveHero(m_myHero, position, fYRotation, true);
 
-			//
 			// 이동 중지
-			//
-
 			m_myHero.EndMove();
 
-			//
 			// 행동 시작
-			//
-
 			m_myHero.StartAction(action);
 
-			//
-			// 응답
-			//
-
+			// 클라이언트 응답 전송
 			SendResponseOK(null);
 		}
 	}
