8f1ba7a2d608cf405e6bfd17d4496755628e2c9b
esh0729 <esh0729@gmail.com>
2022-07-11 07:34:46 +0900

    #. GameServer.HeroMoveCommandHandler 클래스/변수/함수 설명 추가

--- GameServer/Client/Handler/Command/Login/InGame/Move/HeroMoveCommandHandler.cs
+++ GameServer/Client/Handler/Command/Login/InGame/Move/HeroMoveCommandHandler.cs
@@ -7,10 +7,17 @@ using ClientCommon;
 
 namespace GameServer
 {
+	//=====================================================================================================================
+	// (LoginRequiredCommandHandler 상속) 영웅이동 클라이언트 명령을 처리하는 클래스
+	//=====================================================================================================================
 	public class HeroMoveCommandHandler : InGameCommandHandler<HeroMoveCommandBody, HeroMoveResponseBody>
 	{
+		//=====================================================================================================================
+		// 영웅이동 완료 이후 클라이언트 응답 전송을 처리하는 함수
+		//=====================================================================================================================
 		protected override void OnInGameCommandHandle()
 		{
+			// 장소 검사
 			PhysicalPlace currentPlace = m_myHero.currentPlace;
 
 			if (currentPlace == null)
@@ -19,6 +26,7 @@ namespace GameServer
 			if (m_body == null)
 				throw new CommandHandleException(kResult_Error, "body가 null입니다.");
 
+			// 유효성 검사
 			Guid placeInstanceId = m_body.placeInstanceId;
 			Vector3 position = m_body.position;
 			float fYRotation = m_body.yRotation;
@@ -35,21 +43,16 @@ namespace GameServer
 			if (!m_myHero.moving)
 				throw new CommandHandleException(kResult_Error, "영웅이 이동중이 아닙니다.");
 
-			//
 			// 이동
-			//
-
 			InterestedAreaInfo info = currentPlace.MoveHero(m_myHero, position, fYRotation, false);
 
-			//
-			// 응답
-			//
-
+			// 응답객체 생성
 			HeroMoveResponseBody resBody = new HeroMoveResponseBody();
 			resBody.addedHeroes = Hero.ToPDHeroes(info.GetAddedSectorHeroes(m_myHero.id)).ToArray();
 
 			resBody.removedHeroes = info.GetRemovedSectorHeroIds(m_myHero.id).ToArray();
 
+			// 클라이언트 응답 전송
 			SendResponseOK(resBody);
 		}
 	}
